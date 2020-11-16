867e4d01f6c1288eeced901bc10dcfdd2b528575
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-11-16 12:12:13 -0300

    added methods to like

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,3 +2,11 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :tweet
 end
+
+def add_like
+  tweet.update(total_likes: tweet.total_like += 1)
+end
+
+def delete_like
+  tweet.update(total_likes: tweet.total_like -= 1)
+end
