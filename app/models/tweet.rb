441784f64ba1ab1ceb2187c903bec04cecd11875
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-11-16 10:11:05 -0300

    pagination added

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,5 @@
 class Tweet < ApplicationRecord
     belongs_to :user
-    paginates_per 50
 end
 
 # optional: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :tweets
+  paginates_per 50
 end
