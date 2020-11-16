9d74872852bea97c5ce0e4d67277353fe66b019f
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-11-16 12:26:31 -0300

    likes working well

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,12 +1,12 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :tweet
-end
 
-def add_like
-  tweet.update(total_likes: tweet.total_like += 1)
-end
+  def add_like
+    tweet.update(total_likes: tweet.total_likes += 1)
+  end
 
-def delete_like
-  tweet.update(total_likes: tweet.total_like -= 1)
+  def delete_like
+    tweet.update(total_likes: tweet.total_likes -= 1)
+  end
 end
