eb61a3b50da843e6c9bd94b39b4749208138fd92
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-11-18 12:39:57 -0300

    move methods to model

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,5 +3,27 @@ class Tweet < ApplicationRecord
     has_many :likes
 
     validates :content, presence: true
+
+    def retweet
+      @tweet = Tweet.find(params[:id]) if params[:id]
+      rt = Tweet.new(params[:content])
+      rt.user = current_user
+  
+        if rt.save
+          @tweet.update(tweet_params)
+          redirect_to root_path, notice: 'retweeted'
+        else 
+          redirect_to root_path, notice: 'something went wrong'
+        end
+    end
+
+    def add_retweet
+      tweet.update(total_retweets: tweet.total_retweets += 1)
+    end
+
+    def delete_retweet
+      tweet.update(total_retweets: tweet.total_retweets -= 1)
+    end
+    
 end
 
