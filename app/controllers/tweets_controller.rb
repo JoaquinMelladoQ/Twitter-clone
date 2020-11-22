2b727ea1d93ea01ebe4bf25c060e027eaf0a7813
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-11-22 20:14:59 -0300

    added syntax to rt

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -50,7 +50,7 @@ class TweetsController < ApplicationController
     @retweet = @tweet.retweets.new(user: current_user)
     @retweet.save
     new_rt = Tweet.new(content: @tweet.content, user: current_user)
-    new_rt.content += "#{@tweet.user.email}"
+    new_rt.content += ", retweeted by @#{@tweet.user.name}"
     new_rt.save
     redirect_to root_path
   end
